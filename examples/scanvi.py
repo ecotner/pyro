075fddbb5d3876037ff97b66ad7621995d4ac1bb
martin jankowiak <jankowiak@gmail.com>
2020-09-23 02:36:56 +0000

    cleanup

--- examples/scanvi.py
+++ examples/scanvi.py
@@ -1,9 +1,13 @@
+import argparse
+
 import torch
 import torch.nn as nn
 from torch.distributions import constraints
+from torch.nn.functional import softplus
 
 import pyro
 import pyro.distributions as dist
+from pyro.distributions.util import broadcast_shape
 from pyro.optim import Adam
 from pyro.infer import SVI, config_enumerate, TraceEnum_ELBO
 
@@ -17,6 +21,18 @@ def make_fc(dims):
     return nn.Sequential(*layers[:-1])
 
 
+# splits a tensor in half along the final dimension
+def split_in_half(t):
+    return t.reshape(t.shape[:-1] + (2, -1)).unbind(-2)
+
+
+# helper for broadcasting inputs to neural net
+def broadcast_inputs(input_args):
+    shape = broadcast_shape(*[s.shape[:-1] for s in input_args]) + (-1,)
+    input_args = [s.expand(shape) for s in input_args]
+    return input_args
+
+
 # used in parameterizing p(z2 | z1, y)
 class Z2Decoder(nn.Module):
     def __init__(self, z1_dim, y_dim, z2_dim, hidden_dims):
@@ -26,9 +42,8 @@ class Z2Decoder(nn.Module):
 
     def forward(self, z1, y):
         z1_y = torch.cat([z1, y], dim=-1)
-        loc_scale = self.fc(z1_y)
-        loc, scale = loc_scale.reshape(loc_scale.shape[:-1] + (2, -1)).unbind(-2)
-        scale = nn.functional.softplus(scale)
+        loc, scale = split_in_half(self.fc(z1_y))
+        scale = softplus(scale)
         return loc, scale
 
 
@@ -40,10 +55,9 @@ class XDecoder(nn.Module):
         self.fc = make_fc(dims)
 
     def forward(self, z2):
-        gate_mu = self.fc(z2)
-        gate, mu = gate_mu.reshape(gate_mu.shape[:-1] + (2, -1)).unbind(-2)
+        gate, mu = split_in_half(self.fc(z2))
         gate = gate.sigmoid()
-        mu = nn.functional.softplus(mu)
+        mu = softplus(mu)
         return gate, mu
 
 
@@ -55,12 +69,9 @@ class Z2LEncoder(nn.Module):
         self.fc = make_fc(dims)
 
     def forward(self, x):
-        hidden = self.fc(x)
-        hidden_z2, hidden_l = hidden[..., :-2], hidden[..., -2:]
-        z2_loc, z2_scale = hidden_z2.reshape(hidden_z2.shape[:-1] + (2, -1)).unbind(-2)
-        l_loc, l_scale = hidden_l[..., -2], hidden_l[..., -1]
-        z2_scale = nn.functional.softplus(z2_scale)
-        l_scale = nn.functional.softplus(l_scale)
+        h1, h2 = split_in_half(self.fc(x))
+        z2_loc, z2_scale = h1[..., :-1], softplus(h2[..., :-1])
+        l_loc, l_scale = h1[..., -1], softplus(h2[..., -1])
         return z2_loc, z2_scale, l_loc, l_scale
 
 
@@ -72,10 +83,11 @@ class Z1Encoder(nn.Module):
         self.fc = make_fc(dims)
 
     def forward(self, z2, y):
-        z2_y = torch.cat([z2, y], dim=-1)
-        loc_scale = self.fc(z2_y)
-        loc, scale = loc_scale.reshape(loc_scale.shape[:-1] + (2, -1)).unbind(-2)
-        scale = nn.functional.softplus(scale)
+        # this is necessary since Pyro expands y during enumeration
+        z2_y = broadcast_inputs([z2, y])
+        z2_y = torch.cat(z2_y, dim=-1)
+        loc, scale = split_in_half(self.fc(z2_y))
+        scale = softplus(scale)
         return loc, scale
 
 
@@ -91,7 +103,7 @@ class Classifier(nn.Module):
         return logits
 
 
-
+# encompasses the scANVI model and guide as a PyTorch nn.Module
 class SCANVI(nn.Module):
     def __init__(self, num_genes, num_labels, l_loc=0.0, l_scale=1.0, alpha=0.1):
         assert isinstance(num_labels, int) and num_labels > 1
@@ -113,16 +125,13 @@ class SCANVI(nn.Module):
 
         super().__init__()
 
-        self.z2_decoder = Z2Decoder(z1_dim=self.latent_dim,
-                                    y_dim=self.num_labels,
-                                    z2_dim=self.latent_dim,
-                                    hidden_dims=[100, 100])
+        self.z2_decoder = Z2Decoder(z1_dim=self.latent_dim, y_dim=self.num_labels,
+                                    z2_dim=self.latent_dim, hidden_dims=[100, 100])
         self.x_decoder = XDecoder(num_genes=num_genes, hidden_dims=[100, 100], z2_dim=self.latent_dim)
         self.z2l_encoder = Z2LEncoder(num_genes=num_genes, z2_dim=self.latent_dim, hidden_dims=[100, 100])
         self.classifier = Classifier(z2_dim=self.latent_dim, hidden_dims=[100, 100], num_labels=num_labels)
         self.z1_encoder = Z1Encoder(num_labels=num_labels, z1_dim=self.latent_dim,
                                     z2_dim=self.latent_dim, hidden_dims=[100, 100])
-
 
     def model(self, x, y=None):
         pyro.module("scanvi", self)
@@ -140,11 +149,10 @@ class SCANVI(nn.Module):
             l = pyro.sample("l", dist.LogNormal(self.l_loc, self.l_scale))
 
             gate, mu = self.x_decoder(z2)
-            x = pyro.sample("x", dist.ZeroInflatedNegativeBinomial(
-                gate=gate,
-                total_count=1.0 / theta,
-                probs=1.0 / (1.0 + theta * mu),
-            ).to_event(1), obs=x)
+            theta_mu_l = theta * mu * l
+            x_dist = dist.ZeroInflatedNegativeBinomial(gate=gate, total_count=1.0 / theta,
+                                                       probs=theta_mu_l / (1.0 + theta_mu_l))
+            x = pyro.sample("x", x_dist.to_event(1), obs=x)
 
     def guide(self, x, y=None):
         pyro.module("scanvi", self)
@@ -156,31 +164,45 @@ class SCANVI(nn.Module):
             y_logits = self.classifier(z2)
             y_dist = dist.OneHotCategorical(logits=y_logits)
             if y is None:
-                y = pyro.sample(y_dist)
+                # x is unlabeled so sample y using q(y|z2)
+                y = pyro.sample("y", y_dist)
             else:
-                # Add a classification loss term.
+                # x is labeled so add a classification loss term
                 classification_loss = y_dist.log_prob(y)
                 pyro.factor("classification_loss", -self.alpha * classification_loss)
 
-            z1_loc, z1_scale = self.z1_encoder(y, z2)
+            z1_loc, z1_scale = self.z1_encoder(z2, y)
             pyro.sample("z1", dist.Normal(z1_loc, z1_scale).to_event(1))
 
 
-def train():
+def main(args):
+    # clear param store
+    pyro.clear_param_store()
+
     scanvi = SCANVI(num_genes=10, num_labels=7)
+    if args.cuda:
+        scanvi.cuda()
 
     X = torch.randn(51, 10)
     Y = torch.distributions.OneHotCategorical(logits=torch.zeros(7)).sample(sample_shape=(51,))
 
     print("X, Y", X.shape, Y.shape)
 
-    optim = Adam({"lr": 0.01})
+    optim = Adam({"lr": args.learning_rate})
     guide = config_enumerate(scanvi.guide, expand=True)
     svi = SVI(scanvi.model, guide, optim, TraceEnum_ELBO())
 
     svi.step(X, Y)
+    svi.step(X, None)
 
 
 if __name__ == "__main__":
     assert pyro.__version__.startswith('1.4.0')
-    train()
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description="parse args")
+    parser.add_argument('-n', '--num-epochs', default=101, type=int, help='number of training epochs')
+    parser.add_argument('-lr', '--learning-rate', default=1.0e-3, type=float, help='learning rate')
+    parser.add_argument('--cuda', action='store_true', default=False, help='whether to use cuda')
+    args = parser.parse_args()
+
+    main(args)
